f2628bd15a0ce52aa4e4b84b7e3258aa6283caf9
marvelxcodes <ramakrishnan0046@gmail.com>
2023-04-16 17:23:29 +0530

    Added Astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()],
+	integrations: [mdx(), image()],
 });
